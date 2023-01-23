d9b25cbcf93fa78681b77224b5906e1dd33e026e
Silvasss <felipe_silva55@hotmail.com>
2023-01-23 15:54:50 -0300

    versão final

--- app.js
+++ app.js
@@ -4,19 +4,33 @@ const app = express()
 require('dotenv').config()
 require('express-async-errors')
 
+// extra security packages
+const helmet = require('helmet')
+const cors = require('cors')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
+// connectDB 
+const connectDB = require("./db/connect")
+const authenticationUser = require('./middleware/authentication')
+
+// routers
+const authRouter = require('./routes/auth')
+const jobsRouter = require('./routes/jobs')
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
 
 app.use(express.json())
-// extra packages
-
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 // routes
-app.get('/', (req, res) => {
-  res.send('job api')
-})
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/jobs', authenticationUser, jobsRouter)
 
 
 app.use(notFoundMiddleware)
@@ -28,6 +42,8 @@ const port = process.env.PORT || 3000
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
+
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     )
