8c3d2c7a0a652ba9dc343fac80eeb9457ea260ea
Silvasss <felipe_silva55@hotmail.com>
2023-01-31 14:30:36 -0300

    versão final

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express')
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
 const fileUpload = require('express-fileupload')
+const rateLimiter = require('express-rate-limit')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const cors = require('cors')
+const mongoSanitize = require('express-mongo-sanitize')
+
 
 require('express-async-errors')
 require('dotenv').config()
@@ -17,11 +23,25 @@ const authRouter = require('./routes/authRoutes')
 const userRouter = require('./routes/userRoutes')
 const productRouter = require('./routes/productRoutes')
 const reviewRouter = require('./routes/reviewRoutes')
+const orderRouter = require('./routes/orderRoutes')
 
 // middleware
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
+app.set('trust proxy', 1)
+
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+)
+
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+app.use(mongoSanitize())
 // HTTP request logger middleware for node.js
 app.use(morgan('tiny'))
 app.use(express.json())
@@ -38,6 +58,7 @@ app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/products', productRouter)
 app.use('/api/v1/reviews', reviewRouter)
+app.use('/api/v1/orders', orderRouter)
 
 
 app.use(notFoundMiddleware)
